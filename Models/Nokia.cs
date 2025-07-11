e6b401a2f708b64fd9ddbc1d7e2d5c91803d307f
Alexandra Gomes <gomes.marisa.alexa@gmail.com>
2025-07-11 17:42:09 +0100

    Desafio concluídogit add Program.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public sealed class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria) {}
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Baixando aplicação de \"{nomeApp}\" na loja da Nokia...");
+            Console.WriteLine($"\"{nomeApp}\" instalado com sucesso no Nokia {Modelo}!");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia("91234-5678", "Nokia 3310", "IMEI-NOK-001", 8);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine();
+
+
+Smartphone iphone = new Iphone("92345-6789", "iPhone 15", "IMEI-IPH-002", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("ChatGPT");
